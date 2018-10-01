9cc6a970f54ec9eb57140a28958c85744ec1c467
BLACKBAUD\Trevor.Burch <Trevor.Burch@blackbaud.com>
2018-10-01 14:28:06 -0400

    Fixed some index files and removed provisional expecation changes

--- src/app/public/modules/tiles/tile-content/index.ts
+++ src/app/public/modules/tiles/tile-content/index.ts
@@ -1,2 +1,3 @@
 export { SkyTileContentSectionComponent } from './tile-content-section.component';
+export { SkyTileContentComponent } from './tile-content.component';
 export { SkyTileContentModule } from './tile-content.module';
