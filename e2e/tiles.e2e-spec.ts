dfcab827313f3b8c0d93e0ec3398961a53b914a0
Paul Crowder <paul.crowder@blackbaud.com>
2018-11-12 16:00:32 -0500

    Added help button to tile

--- e2e/tiles.e2e-spec.ts
+++ e2e/tiles.e2e-spec.ts
@@ -1,9 +1,25 @@
+// import {
+//   by,
+//   element
+// } from 'protractor';
+
 import {
   expect,
   SkyHostBrowser
 } from '@skyux-sdk/e2e';
 
 describe('Tile', () => {
+  function testFocus(selector: string, done: DoneFn) {
+    SkyHostBrowser.get('visual/tiles');
+    SkyHostBrowser.setWindowBreakpoint('lg');
+
+    // element.all(
+    //   by.css('#screenshot-tiles ' + selector)
+    // ).get(0).sendKeys();
+
+    expect('#screenshot-tiles').toMatchBaselineScreenshot(done);
+  }
+
   it('should match previous screenshot', (done) => {
     SkyHostBrowser.get('visual/tiles');
     SkyHostBrowser.setWindowBreakpoint('lg');
@@ -14,5 +30,21 @@ describe('Tile', () => {
     SkyHostBrowser.get('visual/tiles');
     SkyHostBrowser.setWindowBreakpoint('xs');
     expect('#screenshot-tiles').toMatchBaselineScreenshot(done);
+  });
+
+  it('should match previous screenshot when help button is focused', (done) => {
+    testFocus('.sky-tile-help', done);
+  });
+
+  it('should match previous screenshot when chevron is focused', (done) => {
+    testFocus('.sky-chevron', done);
+  });
+
+  it('should match previous screenshot when settings button is focused', (done) => {
+    testFocus('.sky-tile-settings', done);
+  });
+
+  it('should match previous screenshot when grab handle is focused', (done) => {
+    testFocus('.sky-tile-grab-handle', done);
   });
 });
