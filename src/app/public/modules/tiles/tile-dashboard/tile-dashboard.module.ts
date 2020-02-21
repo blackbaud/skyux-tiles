ed92affa0f8cfe63caed073fe93a75dfc47569ad
Blackbaud-SteveBrush <steve.brush@blackbaud.com>
2020-02-21 16:46:25 -0500

    [rc] Fix Dragula imports

--- src/app/public/modules/tiles/tile-dashboard/tile-dashboard.module.ts
+++ src/app/public/modules/tiles/tile-dashboard/tile-dashboard.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { DragulaService } from 'ng2-dragula/ng2-dragula';
+import { DragulaService } from 'ng2-dragula';
 
 import { SkyTileDashboardComponent } from './tile-dashboard.component';
 import { SkyTileDashboardColumnModule } from '../tile-dashboard-column/tile-dashboard-column.module';
