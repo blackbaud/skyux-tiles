4b55e495fd3b98d02c4af11050b49c4857741a58
Alex <alex.kingman@blackbaud.com>
2021-09-30 16:13:14 -0400

    Merge 5.0.0 next (#85)

--- README.md
+++ README.md
@@ -1,5 +1,3 @@
-# @skyux/tiles
+# @skyux/tiles Workspace
 
-[![npm](https://img.shields.io/npm/v/@skyux/tiles.svg)](https://www.npmjs.com/package/@skyux/tiles)
-![SKY UX CI](https://github.com/blackbaud/skyux-tiles/workflows/SKY%20UX%20CI/badge.svg)
-[![coverage](https://codecov.io/gh/blackbaud/skyux-tiles/branch/master/graphs/badge.svg?branch=master)](https://codecov.io/gh/blackbaud/skyux-tiles/branch/master)
+For information about `@skyux/tiles` please visit the library's README file, located at `projects/tiles/README.md`.
---
+++ projects/tiles-showcase/e2e/src/tiles-theme.e2e-spec.ts
@@ -0,0 +1,24 @@
+import {
+  expect,
+  SkyHostBrowser
+} from '@skyux-sdk/e2e';
+
+describe('Tile with default theme', () => {
+  beforeEach(async () => {
+    await SkyHostBrowser.get('visual/tiles-theme');
+  });
+
+  it('should match previous screenshot', async (done) => {
+    await SkyHostBrowser.setWindowBreakpoint('lg');
+    expect('#screenshot-tiles-theme').toMatchBaselineScreenshot(done, {
+      screenshotName: 'tiles-theme-lg'
+    });
+  });
+
+  it('should match previous screenshot (screen: xs)', async (done) => {
+    await SkyHostBrowser.setWindowBreakpoint('xs');
+    expect('#screenshot-tiles-theme').toMatchBaselineScreenshot(done, {
+      screenshotName: 'tiles-theme-xs'
+    });
+  });
+});
